9246fce8a4ffcceb12611e9143e41b0e10a9f4bd
Damiano <damiano.gianotti@zensor.be>
2025-04-04 11:51:58 +0200

    refactor: clean up code formatting and remove unnecessary whitespace

--- config.py
+++ config.py
@@ -22,24 +22,26 @@ update_G_lr = 0.00005
 LSTM_size = 100
 LSTM_size_layer = 3
 
-def init_config():    
-    global update_G_epoch 
+
+def init_config():
+    global update_G_epoch
     global update_G_lr
 
     predictor_config = dict()
-    predictor_config['lookback_len'] = 3
-    predictor_config['prediction_len'] = 1
-    predictor_config['train_size'] = 5*predictor_config['lookback_len'] + \
-                                     predictor_config['prediction_len'] 
-    
+    predictor_config["lookback_len"] = 3
+    predictor_config["prediction_len"] = 1
+    predictor_config["train_size"] = (
+        5 * predictor_config["lookback_len"] + predictor_config["prediction_len"]
+    )
+
     minimal_threshold = None
-    
-    if 'Tide_pressure' == data_source:
+
+    if "Tide_pressure" == data_source:
         value_range_config = (700, 770)
         minimal_threshold = 0.0036
-        
+
         update_G_epoch = 5
-        update_G_lr = 0.00005        
+        update_G_lr = 0.00005
     elif "Wave_height" == data_source:
         value_range_config = (0, 15.2)
         minimal_threshold = 0.3
@@ -47,8 +49,7 @@ def init_config():
         value_range_config = (-2, 32)
         minimal_threshold = 0.02
     else:
-        print('Unsupported! You need to find out the hyper-parameters by yourselves.')
+        print("Unsupported! You need to find out the hyper-parameters by yourselves.")
         pass
-        
+
     return predictor_config, value_range_config, minimal_threshold
-    
